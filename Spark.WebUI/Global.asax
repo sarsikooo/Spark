528451119043796ded53d111b9c6a15cc7055a3e
sarsikooo <sarsikooo@gmail.com>
2014-10-09 21:16:51 +0200

    initial commit

---
+++ Spark.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Spark.WebUI.MvcApplication" Language="C#" %>
---
+++ Spark.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Spark.WebUI.Startup))]
+namespace Spark.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
